035f091a831b4b136e480383811b83d53517ee68
pirata <pirata@192.168.1.5>
2017-06-21 11:40:14 -0600

    añadiendo la clase de manejos de cadenas

---
+++ src/javagit/ManejoDeCadenas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagit;
+
+import java.util.Date;
+
+/**
+ *
+ * @author pirata
+ */
+public class ManejoDeCadenas {
+   
+    
+    public static void main(String[] arg){
+//String.valueOf convierte el valor recibido al tipo que se llama
+    String numero = String.valueOf(1234); 
+    String fecha = String.valueOf(new Date()); 
+
+    System.out.println(fecha);
+
+    } 
+}
+
+
